77d8ef4d80b235df847f354f71dd249600b5e43c
José Ángel <joseangelmt@outlook.com>
2025-03-17 12:19:44 +0100

    Refactorización

--- transforma_complejo_a_punto.py
+++ transforma_complejo_a_punto.py
@@ -4,8 +4,9 @@
 # El ejemplo transforma dos códigos: BAI_VEG_03_PT y VEG_03_PT
 import digi3d
 
+vista = digi3d.current_view()
+
 def transforma_complejo_a_punto(codigo):
-	vista = digi3d.current_view()
 	eliminar = filter(lambda g: not g.deleted and type(g) is digi3d.Complex and g.codes[0].name == codigo, vista)
 	for entidad in eliminar:
 		centro = ((entidad.min[0] + entidad.max[0])/2, (entidad.min[1] + entidad.max[1])/2, (entidad.min[2] + entidad.max[2])/2)
@@ -13,6 +14,6 @@ def transforma_complejo_a_punto(codigo):
 		vista.add(punto)
 		vista.delete(entidad)
 
-
 transforma_complejo_a_punto('BAI_VEG_03_PT')
 transforma_complejo_a_punto('VEG_03_PT')
+vista.redraw()
